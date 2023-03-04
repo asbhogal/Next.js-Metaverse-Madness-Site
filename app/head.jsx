59c43c88bd20a75b0477a741ec34f560fee22876
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-03-04 15:23:55 +0000

    added assets and postcss config file

--- app/head.jsx
+++ app/head.jsx
@@ -3,7 +3,7 @@ const Head = () => (
       <title>Metaversus</title>
       <meta name="author" content="Aman Singh Bhogal"></meta>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      {/* <link rel="icon" /> */}
+      <link rel="icon" href="./public/favicon.ico"/>
     </>
   );
   
