83bf71b5189cd71a4319f4c2562d773420f25686
Aman Singh Bhogal <79533183+asbhogal@users.noreply.github.com>
2023-03-09 16:25:22 +0000

    started Insights section and omitted nextjs config file

--- sections/Insights.jsx
+++ sections/Insights.jsx
@@ -1,8 +1,30 @@
 'use client';
 
+import { motion } from "framer-motion";
+import styles from "../styles/styles";
+import { staggerContainer } from "../utils/motion";
+import { InsightCard, TitleText, TypingText } from "../components/exports";
+import { insights } from "../constants/data";
+
 const Insights = () => (
-    <section>
-      Insights section
+    <section className={ `${ styles.paddings } relative z-10` }>
+      <motion.div
+        variants={ staggerContainer }
+        initial="hidden"
+        whileInView="show"
+        viewport={ { once: 'false', amount: 25 } }
+        className={ `${ styles.innerWidth } flex flex-col mx-auto` }
+      >
+        <TypingText
+          title="| Insight"
+          textStyles="text-center"
+        />
+
+        <TitleText
+          title="Insight about metaverse"
+          textStyles="text-center"
+        />
+      </motion.div>
     </section>
 );
   
